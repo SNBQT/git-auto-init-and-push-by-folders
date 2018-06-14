1082f11ed5e430adb29489839a74113d443e9498
as <zhangansi@yeah.net>
2018-06-13 20:48:27 -0400

    pull -f

--- init-and-push.sh
+++ init-and-push.sh
@@ -72,7 +72,7 @@ do
   echo $workPath
   cd $workPath
   git pull
-  git pull origin master
+  git pull -f origin master
   git init
   git remote remove git-auto-init-and-push-by-folders
   git remote add git-auto-init-and-push-by-folders https://$username:$password@$domain/$grouppath/$projectPath.git
